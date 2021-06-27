56b3725dff435658513cde3fadaa19e9ee5d5290
GANGADHAR <prasannav@commitservices.net>
2021-06-27 14:39:34 +0530

    server config

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log("nodejs typescript mongodb rest api1")
+import express, { Application, Request, Response, NextFunction } from 'express';
+const app: Application = express();
+const port = 3000;
+const host = "localhost"
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send(`Server running at http://localhost:${port}`)
+});
+
+app.listen(port, host, () => {
+    console.info(`Server listing at http://${host}:${port}`);
+});
